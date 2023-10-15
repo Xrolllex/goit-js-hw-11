3a075f252276e9156bff60f5c33ed208b2975bde
Xrolllex <karolkaczanowski@wp.pl>
2023-10-15 10:56:25 +0200

    Second try

--- src/api.js
+++ src/api.js
@@ -2,17 +2,18 @@ import axios from "axios";
 
 const api_key = "40029898-1d411868c635d026eac66d85b";
 
-const photos = (input, currentPage) => {
-    return axios
-        .get(`https://pixabay.com/api/?key=${api_key}&q=${input}
-        &image_type=photo
-        &orientation=horizontal
-        &safesearch=true
-        &per_page=40
-        &page=${currentPage}`)
-        .then((response) => {
-            return response.data;
-        });
+const photos = async (input, currentPage) => {
+    try {
+        const response = await axios.get(`https://pixabay.com/api/?key=${api_key}&q=${input}
+            &image_type=photo
+            &orientation=horizontal
+            &safesearch=true
+            &per_page=40
+            &page=${currentPage}`);
+        return response.data;
+    } catch (error) {
+        throw error;
+    }
 };
 
 export { photos };
--- src/index.js
+++ src/index.js
@@ -21,75 +21,74 @@ const searchOn = () => {
     }
 };
 
-const createGallery = (event) => {
+const createGallery = async (event) => {
     event.preventDefault();
     searchBtn.setAttribute("disabled", "");
 
     if (checkInput !== input.value) {
-        currentPage = 0
+        currentPage = 0;
         photoArray = [];
+        limit.style.display = "none";
     }
 
-    currentPage++
+    currentPage++;
 
-    photos(encodeURIComponent(input.value), currentPage) 
-        .then((data) => {
-            
-            data.hits.map((element) => {
-                let newPhoto = `
-                    <div class="photo-card">
-                        <a href="${element.largeImageURL}">
-                            <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy"/>
-                        </a>
-                        <div class="info">
-                            <p class="info-item">
+    try {
+        const data = await photos(encodeURIComponent(input.value), currentPage);
+
+        data.hits.map((element) => {
+            let newPhoto = `
+                <div class="photo-card">
+                    <a href="${element.largeImageURL}">
+                        <img src="${element.webformatURL}" alt="${element.tags}" loading="lazy"/>
+                    </a>
+                    <div class="info">
+                        <p class="info-item">
                             <b>Likes </b>${element.likes}
-                            </p>
-                            <p class="info-item">
+                        </p>
+                        <p class="info-item">
                             <b>Views </b>${element.views}
-                            </p>
-                            <p class="info-item">
+                        </p>
+                        <p class "info-item">
                             <b>Comments </b>${element.comments}
-                            </p>
-                            <p class="info-item">
+                        </p>
+                        <p class="info-item">
                             <b>Downloads </b>${element.downloads}
-                            </p>
-                        </div>
-                    </div>`;
-                photoArray.push(newPhoto);
-            });
-
-            if (photoArray.length === 0) {
-                photoBlock.style.display = "block";
-                photoBlock.innerHTML = `<p style="text-align:center;">
-                Sorry, there are no images matching your search query. Please try again.
-                </p>`;
-                loadMore.style.display = "none";
-                Notiflix.Notify.failure(`Sorry, We found ${data.totalHits} images.. Please try again.`);
-            } else {
-                photoBlock.style.display = "flex";
-                photoBlock.innerHTML = photoArray.join("");
-                checkInput = input.value;
-                loadMore.style.display = "block";
-                const photoShow = new SimpleLightbox("a");
-
-                if(currentPage===1){
-                    Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
-                }
-
-                if (photoArray.length >= data.totalHits) {
-                    
-                    limit.style.display = "block";
-                    loadMore.style.display = "none";
-                } else {
-                    currentPage++; 
-                }
-            }
-        })
-        .catch((error) => {
-            console.log(error);
+                        </p>
+                    </div>
+                </div>`;
+            photoArray.push(newPhoto);
         });
+
+        if (photoArray.length === 0) {
+            photoBlock.style.display = "block";
+            photoBlock.innerHTML = `<p style="text-align:center;">
+                Sorry, there are no images matching your search query. Please try again.
+            </p>`;
+            loadMore.style.display = "none";
+            Notiflix.Notify.failure(`Sorry, We found ${data.totalHits} images.. Please try again.`);
+        } else {
+            photoBlock.style.display = "flex";
+            photoBlock.innerHTML = photoArray.join("");
+            checkInput = input.value;
+            loadMore.style.display = "block";
+            const photoShow = new SimpleLightbox("a");
+
+            if (currentPage === 1) {
+                Notiflix.Notify.success(`Hooray! We found ${data.totalHits} images.`);
+            }
+
+            if (photoArray.length >= data.totalHits) {
+                limit.style.display = "block";
+                loadMore.style.display = "none";
+            }
+        }
+    } catch (error) {
+        console.log(error);
+    }
 };
+
+
 
 input.addEventListener("input", searchOn);
 searchBtn.addEventListener("click", createGallery);
